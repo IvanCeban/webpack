48ce843ebff72bad22394d103986b286f42be056
IC <himac!>
2021-04-18 19:36:05 +0300

    3rd party libraries

--- src/analytics.js
+++ src/analytics.js
@@ -1,16 +1,18 @@
+import * as $ from 'jquery'
+
 // Считает клики по документу
 function createAnalytics () {
     let counter = 0
     let isDestroyed = false
     const listener = () => counter++
-    document.addEventListener('click', listener)
+    $(document).on('click', listener)
 
     return {
         destroy() {
-            document.removeEventListener('click', listener)
+            $(document).off('click', listener)
             isDestroyed = true
         },
-        getClicks() {
+        getClicks() {Í
             if(isDestroyed) {
                 return 'Analytics is destroyed. Bye bye!!!'
             }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import * as $ from 'jquery' // импортировать все в переменную $ из библиотеки jquery
 import Post from "@models/Post";
 import json from '@/assets/json'
 import xml from '@/assets/data.xml'
@@ -7,6 +8,7 @@ import WebpackLogo from '@/assets/webpack-logo'
 
 const post = new Post('Webpack Post title.', WebpackLogo)
 
+$('pre').html(post.toString())
 console.log('Post to string:', post.toString())
 
 console.log('JSON: ', json)
